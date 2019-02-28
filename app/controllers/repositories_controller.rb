7054a57e37910e47b20372d11d3598cd17d049a3
Gilbert Torchon <gilbert@noukod.com>
2019-02-28 18:04:15 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,13 @@
 class RepositoriesController < ApplicationController
   
+  
   def index
+    resp=Faraday.get "https://api.github.com/user" do |req|
+      req.params['access_token']="token #{session[:token]}"
+      req.headers['Accept']='application/json'
+    end
+    @body= JSON.parse(resp.body)
   end
+
 
 end

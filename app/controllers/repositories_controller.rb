b8f514e333308102ea73b71e021c893b40f86c56
Gilbert Torchon <gilbert@noukod.com>
2019-02-28 18:15:26 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,6 +6,7 @@ class RepositoriesController < ApplicationController
       req.params['access_token']="token #{session[:token]}"
       req.headers['Accept']='application/json'
     end
+    puts @body["login"]
     @body= JSON.parse(resp.body)
   end
 
